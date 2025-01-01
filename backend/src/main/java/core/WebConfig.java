91ff97297beda58907b390098379cf00bd60ba16
goldfromgoldwila <aryan08012021@gmail.com>
2025-01-02 00:23:20 +0530

    updated cors

--- backend/src/main/java/core/WebConfig.java
+++ backend/src/main/java/core/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://modupdater.onrender.com", "https://modupdater.onrender.com") // Frontend URLs
+                        .allowedOrigins("https://modupdater.onrender.com", "https://goldfromgoldwila.github.io/modUpdater/") // Frontend URLs
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
